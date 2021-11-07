cda74bf56b48ce8c9835ec5ea6e6e24e4a6d3ef9
Kenan Soylu <ken.soylu@hotmail.com>
2021-11-07 15:48:21 +0300

    added .d.ts types

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import dts from "rollup-plugin-dts";
+
 export default [
   {
     input: "./src/web3-store.js",
@@ -5,5 +7,10 @@ export default [
       { file: "dist/index.mjs", format: "es" },
       { file: "dist/index.js", format: "umd", name: "web3store" },
     ]
+  },
+  {
+    input: "./src/svelte-web3.d.ts",
+    output: [{ file: "dist/svelte-web3.d.ts", format: "es" }],
+    plugins: [dts()],
   },
 ]
